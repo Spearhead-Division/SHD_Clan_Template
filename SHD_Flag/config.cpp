b4eee592acb9ab9eb6f6f8b3b0c692a6c01fd7c9
Raychan <davidrueckert@hotmail.de>
2017-09-12 22:54:45 +0200

    Fahnen Mod mit unseren Logo

---
+++ SHD_Flag/config.cpp
@@ -0,0 +1,22 @@
+ class CfgPatches
+ {
+		class shd_flag
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+	};
+ };
+class DefaultEventhandlers;
+class cfgVehicles
+ {
+	 class FlagCarrier;
+	 class shd_flagcarrier: FlagCarrier
+	 {
+		 
+	 
+	 
+	 
+	 };
+ };
