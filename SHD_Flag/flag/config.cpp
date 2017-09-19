88d2aa5654f8ea875855573fda840dc643d6528c
Raychan <davidrueckert@hotmail.de>
2017-09-19 15:18:18 +0200

    Optik

--- SHD_Flag/flag/config.cpp
+++ SHD_Flag/flag/config.cpp
@@ -1,6 +1,7 @@
+ines (34 sloc) 604 Bytes
  class CfgPatches
  {
-		class SHD_FLAG_UI
+	class SHD_FLAG_UI
 	{
 		units[] = {"SHD_FlagCarrierEU"};
 		weapons[] = {};
@@ -10,25 +11,25 @@
 };
 class CfgFactionClasses
 {
-  class SHD_Faction
-  {
-    icon = "\textures\shd_pinup_512x256.paa";
-    displayName = "Fahne [SHD]";
-    priority = 5;
-    side = 1;
-   };
- };
- class CfgVehicles
- {
-  class SHD_FlagCarrierEU;
-  class SHD_Faction: SHD_FlagCarrierEU
-  {
-    scope = 2;
-    accuarcy = 1000;
-    displayName = "";
-    class EventHandlers
-    {
-      init = "(_this select 0) setFlagTexture ""\textures\shd_pinup_512x256.paa""";
-    };
-  };
+	class SHD_Faction
+	{
+		icon = "\textures\shd_pinup_512x256.paa";
+		displayName = "Fahne [SHD]";
+		priority = 5;
+		side = 1;
+	};
+};
+class CfgVehicles
+{
+	class SHD_FlagCarrierEU;
+	class SHD_Faction: SHD_FlagCarrierEU
+	{
+		scope = 2;
+		accuarcy = 1000;
+		displayName = "";
+		class EventHandlers
+		{
+			init = "(_this select 0) setFlagTexture ""\textures\shd_pinup_512x256.paa""";
+		};
+	};
 };
